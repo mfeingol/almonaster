401bb84a139d05b4fc6378238d66e0a4d239eaf8
mfeingol_cp <9204f08b50a98ce38b138b6abea8393a28df75c8xFrFtkcc>
2011-05-30 19:29:44 +0000

    {1/18/2003 6:51:56 PM}620 Final(Committed by Mfeingol)
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS29]$/Almonaster;C3767

--- 620/Final/mkrelease.bat
+++ 620/Final/mkrelease.bat
@@ -3,12 +3,16 @@
 
 cd release
 zip %DEFOPT% -r -D -X -S ..\alajar-win32.zip . -x *.counters -x *.log -x *.dat -x *.report -x *.stat -x *.vcc
+cd ..
 
-gpg --openpgp --output ..\alajar-win32.sig --local-user 3F49A1DD --detach-sign ..\alajar-win32.zip
-gpg --openpgp --verify ..\alajar-win32.sig ..\alajar-win32.zip
+gpg --openpgp --local-user 3F49A1DD --print-md md5 alajar-win32.zip > alajar-win32.md5
+gpg --openpgp --output alajar-win32.md5.asc --local-user 3F49A1DD --clearsign alajar-win32.md5
+gpg --openpgp --verify alajar-win32.md5.asc
 
-zip %DEFOPT% -D ..\alajar-win32-%1.zip ..\alajar-win32.zip ..\alajar-win32.sig
-del ..\alajar-win32.sig
-del ..\alajar-win32.zip
+zip %DEFOPT% -D alajar-win32-%1.zip alajar-win32.zip alajar-win32.md5 alajar-win32.md5.asc
+
+del alajar-win32.zip
+del alajar-win32.md5
+del alajar-win32.md5.asc
 
 @endlocal
--- 620/Final/mksrc.bat
+++ 620/Final/mksrc.bat
@@ -3,13 +3,16 @@
 
 @set DEFOPT=-ST8!n .zip:.jpg:.mp3
 
-zip %DEFOPT% -r -D -X -S alajar-src.zip makefile %devdir%\Admin %devdir%\Alajar %devdir%\AlajarDll %devdir%\AlajarSvc %devdir%\Almonaster %devdir%\AlmonasterHook %devdir%\Asfpp %devdir%\Database %devdir%\OldDatabase %devdir%\DBConv %devdir%\Osal %devdir%\Include -x *.pdb -x *.idb -x *.obj -x *.pch -x *.lib -x *.ilk -x *.exp -x *.dll -x *.exe -x *.plg -x *.opt -x *.ncb -x *.report -x *.stat -x *.dat -x *.log -x *.html -x *.zip -x *.gif -x *.jpg -x *.conf -x *.counters -x *.bmp -x deleteme* -x *.ico -x %devdir%\Alajar\www_root\Almonaster\Resource\* -x *buildlog.htm -x *.enc -x deleteme*.txt -x *.scc -x *.vspscc -x *.suo
+zip %DEFOPT% -r -D -X -S alajar-src.zip makefile %devdir%\Admin %devdir%\Alajar %devdir%\AlajarDll %devdir%\AlajarSvc %devdir%\Almonaster %devdir%\AlmonasterHook %devdir%\Asfpp %devdir%\Database %devdir%\OldDatabase %devdir%\DBConv %devdir%\Osal %devdir%\Include -x *.pdb -x *.idb -x *.obj -x *.pch -x *.lib -x *.ilk -x *.exp -x *.dll -x *.exe -x *.plg -x *.opt -x *.ncb -x *.report -x *.stat -x *.dat -x *.log -x *.html -x *.zip -x *.gif -x *.jpg -x *.conf -x *.counters -x *.bmp -x deleteme* -x *.ico -x %devdir%\Alajar\www_root\Almonaster\Resource\* -x *buildlog.htm -x *.enc -x deleteme*.txt -x *.scc -x *.vspscc -x *.vssscc -x *.suo
 
-gpg --openpgp --output alajar-src.sig --local-user 3F49A1DD --detach-sign alajar-src.zip
-gpg --openpgp --verify alajar-src.sig alajar-src.zip
+gpg --openpgp --local-user 3F49A1DD --print-md md5 alajar-src.zip > alajar-src.md5
+gpg --openpgp --output alajar-src.md5.asc --local-user 3F49A1DD --clearsign alajar-src.md5
+gpg --openpgp --verify alajar-src.md5.asc
 
-zip %DEFOPT% -D alajar-src-%1.zip alajar-src.zip alajar-src.sig
-del alajar-src.sig
+zip %DEFOPT% -D alajar-src-%1.zip alajar-src.zip alajar-src.md5 alajar-src.md5.asc
+
 del alajar-src.zip
+del alajar-src.md5
+del alajar-src.md5.asc
 
 @endlocal
