7032347513c30a39a172efcb6a55ce5687e95c31
mfeingol_cp <9204f08b50a98ce38b138b6abea8393a28df75c8xFrFtkcc>
2011-05-30 19:29:42 +0000

    {12/23/2002 11:57:24 AM}(Committed by Mfeingol)
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS29]$/Almonaster;C3765

--- 620/Final/mkrelease.bat
+++ 620/Final/mkrelease.bat
@@ -1,10 +1,14 @@
 @setlocal
-@set devdir=D:\mfeingol\Dev
-
-@set DEFOPT=-ST9!n .zip:.jpg:.mp3
+@set DEFOPT=-ST8!n .zip:.jpg:.mp3
 
 cd release
-zip %DEFOPT% -r -D %devdir%\alajar-win32.zip . -x *.counters -x *.log -x *.dat -x *.report -x *.stat
-cd ..
+zip %DEFOPT% -r -D -X -S ..\alajar-win32.zip . -x *.counters -x *.log -x *.dat -x *.report -x *.stat -x *.vcc
+
+gpg --openpgp --output ..\alajar-win32.sig --local-user 3F49A1DD --detach-sign ..\alajar-win32.zip
+gpg --openpgp --verify ..\alajar-win32.sig ..\alajar-win32.zip
+
+zip %DEFOPT% -D ..\alajar-win32-%1.zip ..\alajar-win32.zip ..\alajar-win32.sig
+del ..\alajar-win32.sig
+del ..\alajar-win32.zip
 
 @endlocal
--- 620/Final/mksrc.bat
+++ 620/Final/mksrc.bat
@@ -1,8 +1,15 @@
 @setlocal
 @set devdir=.
 
-@set DEFOPT=-ST9!n .zip:.jpg:.mp3
+@set DEFOPT=-ST8!n .zip:.jpg:.mp3
 
-zip %DEFOPT% -r -D alajar-src.zip makefile %devdir%\Admin %devdir%\Alajar %devdir%\AlajarDll %devdir%\AlajarSvc %devdir%\Almonaster %devdir%\AlmonasterHook %devdir%\Asfpp %devdir%\Database %devdir%\OldDatabase %devdir%\DBConv %devdir%\Osal %devdir%\Include -x *.pdb -x *.idb -x *.obj -x *.pch -x *.lib -x *.ilk -x *.exp -x *.dll -x *.exe -x *.plg -x *.opt -x *.ncb -x *.report -x *.stat -x *.dat -x *.log -x *.html -x *.zip -x *.gif -x *.jpg -x *.conf -x *.counters -x *.bmp -x deleteme* -x *.ico -x %devdir%\Alajar\www_root\Almonaster\Resource\AlienUploads\* -x *buildlog.htm -x *.enc -x deleteme*.txt
+zip %DEFOPT% -r -D -X -S alajar-src.zip makefile %devdir%\Admin %devdir%\Alajar %devdir%\AlajarDll %devdir%\AlajarSvc %devdir%\Almonaster %devdir%\AlmonasterHook %devdir%\Asfpp %devdir%\Database %devdir%\OldDatabase %devdir%\DBConv %devdir%\Osal %devdir%\Include -x *.pdb -x *.idb -x *.obj -x *.pch -x *.lib -x *.ilk -x *.exp -x *.dll -x *.exe -x *.plg -x *.opt -x *.ncb -x *.report -x *.stat -x *.dat -x *.log -x *.html -x *.zip -x *.gif -x *.jpg -x *.conf -x *.counters -x *.bmp -x deleteme* -x *.ico -x %devdir%\Alajar\www_root\Almonaster\Resource\* -x *buildlog.htm -x *.enc -x deleteme*.txt -x *.scc -x *.vspscc -x *.suo
+
+gpg --openpgp --output alajar-src.sig --local-user 3F49A1DD --detach-sign alajar-src.zip
+gpg --openpgp --verify alajar-src.sig alajar-src.zip
+
+zip %DEFOPT% -D alajar-src-%1.zip alajar-src.zip alajar-src.sig
+del alajar-src.sig
+del alajar-src.zip
 
 @endlocal
