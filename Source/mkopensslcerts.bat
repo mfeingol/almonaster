25986672a0ca3398e1aef658910a6cd2cbd2e871
Max Feingold <mfeingol@outlook.com>
2019-11-25 19:16:11 -0800

    Update script

--- Source/mkopensslcerts.bat
+++ Source/mkopensslcerts.bat
@@ -1,4 +1,4 @@
-REM E = almonaster@myrealbox.com, CN = prometheus.almonaster.net, OU = Almonaster, L = Bellevue, WA, C = US
+REM E = root@almonaster.net, CN = falcon.almonaster.net, OU = Almonaster, L = Redmond, WA, C = US
 
 openssl genrsa -out server.key 2048
 openssl req -new -x509 -key server.key -out cert.pem -days 1825
